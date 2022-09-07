00f13170cfbb1193e2ece42b48a5b196893810b2
Cristina Zamora <ecoandrea@hotmail.com>
2022-09-07 00:24:21 -0300

    🍏

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import axios from 'axios'
+
+const getData = async () => {
+  try{
+const {data} = await axios.get("https://api-dolar-olg2.onrender.com")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getData()
+</script>
+
+<template>
+  <h1>Api Dolar</h1>
+</template>
