75bf3dd4b7b661c5f96c5d4e86cd930de05d7235
Cristina Zamora <ecoandrea@hotmail.com>
2022-09-07 00:50:42 -0300

    con dist

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <script setup>
-import axios from 'axios'
-
-const getData = async () => {
-  try{
-const {data} = await axios.get("https://api-dolar-olg2.onrender.com")
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-getData()
-</script>
-
-<template>
-  <h1>Api Dolar</h1>
-</template>
+  import axios from "axios";
+  import { ref } from "vue";
+  const dolar = ref("");
+  const uf = ref("");
+  const fecha = ref("");
+  const loading = ref(true);
+  const getData = async () => {
+      loading.value = true;
+      try {
+          const { data } = await axios.get(
+              "https://api-dolar-kib9.onrender.com/"
+          );
+          // console.log(data);
+          dolar.value = data.dolar;
+          uf.value = data.UF;
+          fecha.value = data.fecha;
+      } catch (error) {
+          console.log(error);
+      } finally {
+          loading.value = false;
+      }
+  };
+  getData();
+  </script>
+  
+  <template>
+      <h1 v-if="loading">Loading data...</h1>
+      <h1 v-else>
+          Api Dolar Fecha: {{ fecha }} - Dolar: {{ dolar }} - UF: {{ uf }}
+      </h1>
+  </template>
