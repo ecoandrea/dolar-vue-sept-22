d70463b1bcf825d20365d79205d2d4af2275fe10
Cristina Zamora <ecoandrea@hotmail.com>
2022-09-09 15:40:48 -0300

    dia 2

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <script setup>
-  import axios from "axios";
-  import { ref } from "vue";
-  const dolar = ref("");
-  const uf = ref("");
-  const fecha = ref("");
-  const loading = ref(true);
-  const getData = async () => {
-      loading.value = true;
-      try {
-          const { data } = await axios.get(
-              "https://api-dolar-kib9.onrender.com/"
-          );
-          // console.log(data);
-          dolar.value = data.dolar;
-          uf.value = data.UF;
-          fecha.value = data.fecha;
-      } catch (error) {
-          console.log(error);
-      } finally {
-          loading.value = false;
-      }
-  };
-  getData();
-  </script>
-  
-  <template>
-      <h1 v-if="loading">Loading data...</h1>
-      <h1 v-else>
-          Api Dolar Fecha: {{ fecha }} - Dolar: {{ dolar }} - UF: {{ uf }}
-      </h1>
-  </template>
+import CardMoney from "./components/CardMoney.vue";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+<Navbar />
+
+  <div class="container mx-auto px-4">
+    <h1 class="font-light text-center text-blue-600">API DOLAR</h1>
+    <main class="grid grid-cols-1 sm:grid-cols-4 gap-2 pt-5">
+      <CardMoney />
+      <CardMoney />
+      <CardMoney />
+      <CardMoney />
+    </main>
+  </div>
+</template>
---
+++ src/components/CardMoney.vue
@@ -0,0 +1,27 @@
+<script setup>
+import IconDolar from "./icons/IconDolar.vue";
+import IconUp from "./icons/IconUp.vue";
+import IconDown from "./icons/IconDown.vue";
+</script>
+
+<template>
+  <article
+    class="relative p-4 text-center text-gray-100 bg-gray-800 rounded-md"
+  >
+    <span
+      class="absolute top-0 left-0 right-0 h-1 bg-white rounded-t-md bg-gradient-to-r from-pink-300 to-pink-600"
+    ></span>
+    <div
+      class="absolute flex justify-center w-8 h-8 -translate-x-1/2 bg-gray-500 rounded-full -top-4 left-1/2"
+    >
+      <IconDolar class="w-3 fill-gray-100" />
+    </div>
+    <h2>Dolar</h2>
+    <h3 class="py-4 text-4xl">$850</h3>
+    <p class="flex justify-center gap-1">
+      <IconUp class="w-2 fill-green-500" />
+      <IconDown class="w-2 fill-red-600" />
+      <span>0.05%</span>
+    </p>
+  </article>
+</template>
